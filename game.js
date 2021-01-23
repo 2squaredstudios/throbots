7cdf66e81b9fc5c906fe673e0c09521bcb9ad58f
Lennon McLean <lmmclean08@gmail.com>
2021-01-23 09:51:03 -0500

    added gitignore and added movement

--- game.js
+++ game.js
@@ -1,6 +1,48 @@
 var ctx = $('#canvas').getContext('2d');
 var mike = new Image();
+var x = 100;
+var y = 100;
+var ground = 750;
+var yvelocity = 0;
+var adown = false;
+var ddown = false;
 mike.src = 'mike.png';
-mike.onload = function() {
-  ctx.drawImage(mike, 100, 100);
+setInterval(function() {
+  y -= yvelocity;
+  $('#canvas').width = $('#canvas').width;
+  ctx.drawImage(mike, x, y);
+  if (y > ground) {
+    yvelocity = 0;
+    y = ground + 1;
+  }
+  else {
+    yvelocity -= 0.5;
+  }
+  if (adown) {
+    x-= 5;
+  }
+  if (ddown) {
+    x += 5;
+  }
+});
+document.onkeydown = function(event) {
+  if (yvelocity == 0) {
+    if (event.code == 'KeyW') {
+      yvelocity = 20;
+    }
+  }
+  if (event.code == 'KeyA') {
+    adown = true;
+  }
+  if (event.code == 'KeyD') {
+    ddown = true;
+  }
+}
+document.onkeyup = function(event) {
+  if (event.code == 'KeyA') {
+    adown = false;
+  }
+  if (event.code == 'KeyD') {
+    ddown = false;
+  }
 }
