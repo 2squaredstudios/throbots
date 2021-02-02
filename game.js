1d3b7c708cdbb95ba6365a74ed3b0cafa598f8f6
Lennon McLean <lmmclean08@gmail.com>
2021-02-01 20:30:35 -0500

    fixed images, added infinite scroll, platforms, and fps

--- game.js
+++ game.js
@@ -1,34 +1,44 @@
 var ctx = $('#canvas').getContext('2d');
+var fpscounter = 0;
+var fpslimit = 0;
 var mike = new Image();
-var x = 100;
+mike.src = 'mike2.png';
+var platform = new Image();
+platform.src = 'platform.png';
+var platforms = [{x: 100, y: 600}, {x: 0, y: 800}, {x: 100, y: 800}, {x: 200, y: 800}, {x: 300, y: 800}, {x: 400, y: 800}, {x: 500, y: 800}, {x: 600, y: 800}, {x: 700, y: 800}, {x: 800, y: 800}, {x: 900, y: 800}, {x: 1000, y: 800}];
+var x = 101;
 var y = 100;
-var ground = 600;
 var yvelocity = 0;
 var adown = false;
 var ddown = false;
-mike.src = 'mike.png';
-setInterval(function() {
-  y -= yvelocity;
+function loop() {
+  fpscounter++;
+  y += yvelocity;
   $('#canvas').width = $('#canvas').width;
-  ctx.drawImage(mike, x, y);
-  if (y > ground) {
-    yvelocity = 0;
-    y = ground + 1;
+  var condition = false;
+  for (var i = 0; i < platforms.length; i++) {
+    ctx.drawImage(platform, platforms[i].x - x + 550, platforms[i].y);
+    if ((x > platforms[i].x) && (x < (platforms[i].x + 100)) && (y > platforms[i].y) && (y < (platforms[i].y + 100))) {
+      condition = true;
+      yvelocity = 0;
+      y = platforms[i].y + 1;
+    }
   }
-  else {
-    yvelocity -= 0.5;
+  if (!condition) {
+    yvelocity += 0.5;
   }
+  ctx.drawImage(mike, 500, y - 150);
   if (adown) {
     x-= 5;
   }
   if (ddown) {
     x += 5;
   }
-});
+}
 document.onkeydown = function(event) {
   if (yvelocity == 0) {
     if (event.code == 'KeyW') {
-      yvelocity = 20;
+      yvelocity = -20;
     }
   }
   if (event.code == 'KeyA') {
@@ -40,6 +50,7 @@ document.onkeydown = function(event) {
     mike.src = 'mike.png';
   }
 }
+var gameloop = setInterval(loop, fpslimit);
 document.onkeyup = function(event) {
   if (event.code == 'KeyA') {
     adown = false;
@@ -47,4 +58,20 @@ document.onkeyup = function(event) {
   if (event.code == 'KeyD') {
     ddown = false;
   }
+}
+setInterval(function() {
+  $('#fps').innerHTML = fpscounter;
+  fpscounter = 0;
+}, 1000);
+$('#fpslimiter').oninput = function() {
+  if ($('#fpslimiter').value == 301) {
+    $('#maxfps').innerHTML = 'Unlimited';
+    fpslimit = 0;
+  }
+  else {
+    $('#maxfps').innerHTML = $('#fpslimiter').value;
+    fpslimit = 1000 / parseInt($('#fpslimiter').value);
+  }
+  clearInterval(gameloop);
+   gameloop = setInterval(loop, fpslimit);
 }
