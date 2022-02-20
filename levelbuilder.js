224f4e6ede6d9e2b64a2133deafc9305fd4afea2
Lennon McLean <lmmclean08@gmail.com>
2022-02-19 23:17:34 -0500

    completed level builder

--- levelbuilder.js
+++ levelbuilder.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
+var selection = 'platform';
 var ctx = $('#canvas').getContext('2d');
 var platforms = [];
 var entities = {};
 var rect = $('#canvas').getBoundingClientRect();
+var entity = new Image();
 var background = new Image();
 var platform = new Image();
 var mousepos = {x: 0, y: 0};
+entity.src = 'images/entitytemplate.png';
 background.src = 'images/' + $('input[name="theme"]:checked').value + '.png';
 platform.src = 'images/' + $('input[name="theme"]:checked').value + 'platform.png';
 background.onload = function() {
@@ -14,6 +17,12 @@ background.onload = function() {
   ctx.drawImage(background, 400, 0);
   ctx.drawImage(background, 600, 0);
   ctx.drawImage(background, 800, 0);
+}
+function selectplatform() {
+  selection = 'platform';
+}
+function selectentity() {
+  selection = 'entity';
 }
 setInterval(function() {
   if (!background.src.includes($('input[name="theme"]:checked').value)) {
@@ -29,13 +38,28 @@ setInterval(function() {
   for (var i = 0; i < platforms.length; i++) {
     ctx.drawImage(platform, platforms[i].x, platforms[i].y);
   }
-  ctx.drawImage(platform, mousepos.x, mousepos.y);
+  for (var entityid in entities) {
+    ctx.drawImage(entity, entities[entityid].x, entities[entityid].y);
+  }
+  if (selection == 'platform') {
+    ctx.drawImage(platform, mousepos.x, mousepos.y);
+  }
+  else {
+    ctx.drawImage(entity, mousepos.x, mousepos.y);
+  }
 });
 document.onmousemove = function(event) {
   mousepos = {x: (event.clientX - rect.left) / 2, y: (event.clientY - rect.top) / 2};
 }
 $('#canvas').onclick = function(event) {
-  platforms.push({x: Math.round((event.clientX - rect.left) / 2), y: Math.round((event.clientY - rect.top) / 2)});
+  var clickX = Math.round((event.clientX - rect.left) / 2);
+  var clickY = Math.round((event.clientY - rect.top) / 2);
+  if (selection == 'platform') {
+    platforms.push({x: clickX, y: clickY});
+  }
+  else {
+    entities[prompt('Enter entity name')] = {x: clickX, y: clickY, yvelocity: 0, xvelocity: 0, crouchdown: false, leftdown: false, rightdown: false, frame: prompt('Enter frame'), thrown: false};
+  }
 }
 function save() {
   fs.writeFile(prompt('Enter world file'), JSON.stringify({title: $('#name').value, entities: entities, platforms: platforms, theme: $('input[name="theme"]:checked').value}), function(err) {
