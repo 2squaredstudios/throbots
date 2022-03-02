870ffc60c7b7ae0c8410fc833534a8e2d067fb75
Lennon McLean <lmmclean08@gmail.com>
2022-03-01 20:47:49 -0500

    changed lennet config

--- lennetserver.js
+++ lennetserver.js
@@ -1,2 +1,2 @@
 var lennet = require('@thecoder08/lennet');
-lennet.init(25568, 'node server.js %PORT% 60 world.json');
+lennet.init(25568, 'screen -d -m -S throbots-instance-%PORT% node server.js %PORT% 60 world.json');
