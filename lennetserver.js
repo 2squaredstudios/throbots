918f72bb900b331bf0ccb001bde85cb4ad3acb22
Lennon McLean <lmmclean08@gmail.com>
2023-08-08 12:36:13 -0400

    logging

--- lennetserver.js
+++ lennetserver.js
@@ -1,2 +1,1 @@
-var lennet = require('@thecoder08/lennet');
-lennet.init(25568, 'screen -d -m -S throbots-instance-%PORT% node server.js %PORT% 60 world.json true');
+require('@thecoder08/lennet').init(25568, 'screen -L -Logfile throbots-instance-%PORT%.log -d -m -S throbots-instance-%PORT% node server.js %PORT% 60 world.json true');
